4ad0825528bd4b12d6aa345c6690992ba4a06c2c
yehor <yegor_litsov@iownit.us>
2020-06-29 20:54:16 +0300

    Functionality and styles done, added base animation

--- src/app/list/modules/custom-input/custom-input.component.ts
+++ src/app/list/modules/custom-input/custom-input.component.ts
@@ -1,6 +1,7 @@
-import {ChangeDetectionStrategy, Component, forwardRef} from '@angular/core';
+import {ChangeDetectionStrategy, Component, forwardRef, Input} from '@angular/core';
 import {FormControl, NG_VALUE_ACCESSOR} from '@angular/forms';
 import {BaseInputDirective} from '../../directives/base-input.directive';
+import {animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-custom-input',
@@ -14,5 +15,29 @@ import {BaseInputDirective} from '../../directives/base-input.directive';
       multi: true,
     },
   ],
+  animations: [
+    trigger(
+      'inOutAnimation',
+      [
+        transition(
+          ':enter',
+          [
+            style({ opacity: 0 }),
+            animate('.3s ease-out', style({ opacity: 1 }))
+          ]
+        ),
+        transition(
+          ':leave',
+          [
+            style({ opacity: 1 }),
+            animate('.1s ease-in', style({ opacity: 0 }))
+          ]
+        )
+      ]
+    )
+  ]
 })
-export class CustomInputComponent extends BaseInputDirective<FormControl> {}
+export class CustomInputComponent extends BaseInputDirective<FormControl> {
+  @Input() placeholder?: string;
+  @Input() errorMessage?: string;
+}
--- src/app/list/modules/list/list.component.ts
+++ src/app/list/modules/list/list.component.ts
@@ -2,12 +2,34 @@ import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {FormArray, FormBuilder, Validators} from '@angular/forms';
 import {OptionInterface} from '../../interfaces';
 import {RuleOption} from '../../enums';
+import {animate, style, transition, trigger} from '@angular/animations';
 
 @Component({
   selector: 'app-list',
   templateUrl: './list.component.html',
   styleUrls: ['./list.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger(
+      'blockAdd',
+      [
+        transition(
+          ':enter',
+          [
+            style({ opacity: 0, height: 0 }),
+            animate('.3s ease-out', style({ opacity: 1 }))
+          ]
+        ),
+        transition(
+          ':leave',
+          [
+            style({ opacity: 1 }),
+            animate('.1s ease-in', style({ opacity: 0 }))
+          ]
+        )
+      ]
+    )
+  ]
 })
 export class ListComponent {
 
@@ -34,7 +56,7 @@ export class ListComponent {
 
   addCampaignRule(): void {
     const campaignRule = this.formBuilder.group({
-      ruleOption: [null],
+      ruleOption: [RuleOption.Contains],
       urls: [null, Validators.required],
     });
 
